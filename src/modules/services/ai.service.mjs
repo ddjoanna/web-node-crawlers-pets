a2470a66bf5893ad68ff85609adcb20b48b81993
ddjoanna <joann.chang0722@gmail.com>
2025-04-24 19:19:54 +0800

    feat: implement AI service abstraction with OpenAI and Gemini support

---
+++ src/modules/services/ai.service.mjs
@@ -0,0 +1,26 @@
+import retryRequest from "../utils/retry.util.mjs";
+import gemini_ai from "../components/google_gen_ai.component.mjs";
+import open_ai from "../components/open_ai.component.mjs";
+import config from "../config/index.mjs";
+
+const provider = config.AI.provider;
+
+class AiService {
+  constructor() {
+    this.service = provider === "gemini" ? gemini_ai : open_ai;
+  }
+
+  async generateContent(prompt) {
+    return retryRequest(async () => {
+      return await this.service.generateContent(prompt);
+    });
+  }
+
+  async generateEmbeddings(prompt) {
+    return retryRequest(async () => {
+      return await this.service.generateEmbeddings(prompt);
+    });
+  }
+}
+
+export default new AiService();
